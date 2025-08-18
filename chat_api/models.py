9cac4a0789230ba75cde0be45ab9ccab379ca3fb
l1f3-sh <peternwaofom754@gmail.com>
2025-08-18 13:48:05 +0100

    Initialize Django project structure with basic settings, URLs, and ASGI/WSGI configurations

---
+++ chat_api/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    """
+    Custom User model inheriting from Django's AbstractUser.
+    This gives us all the fields of a standard User (username, password, email, etc.)
+    and we can add our custom fields on top.
+    """
+    username = models.CharField(max_length=50, unique=True)
+    password = models.CharField(max_length=128)
+    tokens = models.IntegerField(default=4000)
+
+class Chat(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    message = models.TextField()
+    respomse = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    
